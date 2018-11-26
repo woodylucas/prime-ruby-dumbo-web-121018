8962801f40025d393833aa4a7e31b710d47d5662
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 04:50:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+  return false if number < 2
+  return true if number == 2
+  (2..number -1).each do |x| #loop over a range of 2 to one less than the number
+    #now test if number divided by x has no remainder:
+    return false if number % x == 0
+  end
+  return true
+end
