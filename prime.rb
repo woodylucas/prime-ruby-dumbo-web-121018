24f13d5b28ba6fa7b9f59655ae550e35ef16ed44
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 04:24:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,7 @@
 # Add  code here!
 def prime?(num)
-
-  if num == 1
+  if num == 1 
     false
-  end
-  if num == 2
-    true
-  end
-  is_prime = true
-  for i in 2...num do
-    #puts "#{i}"
-    if num % i == 0
-      is_prime = false
-      break
-    end
-  end
-
-  is_prime
-
-end
+    elsif num == 4 
+  end 
+end 
